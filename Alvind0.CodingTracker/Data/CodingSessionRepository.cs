80b2a56974c80367e1723c00704e0a2b3128ad28
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2024-12-12 14:58:52 +0900

    Add project files

---
+++ Alvind0.CodingTracker/Data/CodingSessionRepository.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Alvind0.CodingTracker.Models;
+using System.Net.NetworkInformation;
+namespace Alvind0.CodingTracker.Data;
+
+
+public class CodingSessionRepository
+{
+    private readonly string _connectionString;
+
+    public CodingSessionRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public SqliteConnection GetConnection()
+    {
+        return new SqliteConnection(_connectionString);
+    }
+
+    // Initialize tables: "Coding Sessions" & "Goals" If it doesn't exist yet 
+    public void CreateTables()
+    {
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+
+            var query = @"
+            CREATE TABLE IF NOT EXISTS 'Coding Sessions'(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            StartTime TEXT NOT NULL,
+            EndTime TEXT NOT NULL,
+            Duration INTEGER);
+            CREATE TABLE IF NOT EXISTS Goals(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Goal INTEGER NOT NULL,
+            StartDate TEXT NOT NULL,
+            EndDate TEXT,
+            Progress INTEGER,
+            IsCompleted INTEGER);
+            ";
+
+            connection.Execute(query);
+        }
+    }
+
+    public void AddSession(DateTime start, DateTime end)
+    {
+        var session = new CodingSession
+        {
+            StartTime = start,
+            EndTime = end
+        };
+
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+            var query = @"
+INSERT INTO 'Coding Sessions' (StartTime, EndTime, Duration) 
+VALUES (@StartTime, @EndTime, @Duration);";
+            connection.Execute(query, session);
+        }
+    }
+
+    public IEnumerable<CodingSession> GetCodingSessions()
+    {
+        using (var connection = GetConnection())
+        {
+            var query = @"SELECT * FROM 'Coding Sessions'";
+            var sessions = connection.Query<CodingSession>(query);
+            return sessions;
+        }
+    }
+}
---
+++ Alvind0.CodingTracker/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Alvind0.CodingTracker.Views;
+using Alvind0.CodingTracker.Data;
+using Alvind0.CodingTracker.Controllers;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("//Alvind0.CodingTracker//Properties//appsettings.json")
+    .Build();
+
+var connectionString = configuration.GetSection("ConnectionStrings")["DefaultConnection"] 
+    ?? throw new NullReferenceException("Connection String does not exist in the configuration file.");
+
+var database = new CodingSessionRepository(connectionString);
+var sessionController = new CodingSessionController(database);
+var menu = new Menu(sessionController);
+database.CreateTables();
+menu.MainMenu();
