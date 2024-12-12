bb3a7fab92ddf2391db221c5303ee7742aee06e6
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2024-12-12 14:58:22 +0900

    Add CodingSessionController for input validation and for Menu to interface with DB

---
+++ Alvind0.CodingTracker/Controllers/CodingSessionController.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+using Alvind0.CodingTracker.Data;
+using Spectre.Console;
+namespace Alvind0.CodingTracker.Controllers;
+
+public class CodingSessionController
+{
+    private readonly CodingSessionRepository _repository;
+
+    public CodingSessionController(CodingSessionRepository repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException("Repository does not exist.");
+    }
+
+
+    public void LogSessionManually()
+    {
+        DateTime startTime, endTime;
+        while (true)
+        {
+            startTime = GetTime("Insert Start Time (Format: MM-dd-yy H:mm): ");
+            endTime = GetTime("Insert End Time (Format: MM-dd-yy H:mm): ");
+            if (startTime > endTime)
+            {
+                Console.Clear();
+                Console.WriteLine("Start Time cannot be later than End Time");
+                continue;
+            }
+            break;
+        }
+        _repository.AddSession(startTime, endTime);
+    }
+
+    public DateTime GetTime(string message)
+    {
+        while (true)
+        {
+            var time = AnsiConsole.Ask<string>(message);
+            if (DateTime.TryParseExact(time, "MM-dd-yy H:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+            {
+                return result;
+            }
+            Console.Clear();
+            Console.WriteLine("Please enter in the correct format.");
+        }
+    }
+
+    public void ShowCodingSessions()
+    {
+        var sessions = _repository.GetCodingSessions();
+
+        var table = new Table();
+        table.Border = TableBorder.Rounded;
+        table.AddColumns("Id", "Start Time", "EndTime", "Duration");
+
+        foreach (var session in sessions)
+        {
+            table.AddRow(
+                session.Id.ToString(),
+                session.StartTime.ToString("MM-dd-yy H:mm"),
+                session.EndTime?.ToString("MM-dd-yy H:mm") ?? "Invalid Time.",
+                session.Duration.ToString(@"hh\:mm")
+                );
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
